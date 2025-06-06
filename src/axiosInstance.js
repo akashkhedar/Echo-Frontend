81294d103df7c52a6735d3f8368e28332a60d584
akashkhedar <akashkhedar262@gmail.com>
2025-06-06 22:13:56 +0530

    deployment

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000", // Your backend URL
+  baseURL: "https://echo-backend-4am1.onrender.com", // Your backend URL
   withCredentials: true, // This will include cookies in all requests
 });
 
